fd41a575406c9d3455d0fd5b682ab46228285461
padovaSR <34258342+padovaSR@users.noreply.github.com>
2018-01-05 00:04:16 +0100

    Create na_functions.py

---
+++ na_functions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import pysrt
+import zipfile, zlib
+import sys
+import os
+
+class fileOpened(object):
+    def isCompressed(self, infile):
+        if zipfile.is_zipfile(infile) == True:
+            print("ZIP archive: {}".format(os.path.basename(infile)))
+            with zipfile.ZipFile(infile, 'r') as zf:
+                print(zf.namelist())
