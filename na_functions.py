1e990de9b4ebf8c61e660e330017edd6b4973e4e
padovaSR <34258342+padovaSR@users.noreply.github.com>
2018-01-06 22:59:39 +0100

    Update na_functions.py

--- na_functions.py
+++ na_functions.py
@@ -5,10 +5,34 @@ import zipfile
 import sys
 import os
 
+import wx
+
 class fileOpened(object):
     def isCompressed(self, infile):
-        if zipfile.is_zipfile(infile) == True:
-            print("ZIP archive: {}".format(os.path.basename(infile)))
-            with zipfile.ZipFile(infile, 'r') as zf:
-                print(zf.namelist())
-                
+        basepath = os.path.split(infile)[0]
+        imeFajla = os.path.split(infile)[-1]
+        with zipfile.ZipFile(infile, 'r') as zf:
+            singlefajl = len(zf.namelist())
+            if singlefajl == 1:
+                jedanFajl = zf.namelist()[0]
+                print(jedanFajl)
+                outfile = os.path.join(basepath, jedanFajl)
+                with open(outfile, 'wb') as f:
+                    f.write(zf.read(jedanFajl))
+            elif singlefajl > 1:
+                izbor = [x for x in zf.namelist() if not x.endswith('/')]
+                dlg = wx.SingleChoiceDialog(None, 'Pick one:', imeFajla, izbor)
+                if dlg.ShowModal() == wx.ID_OK:
+                    respond = dlg.GetStringSelection()
+                    if respond:
+                        namepath = os.path.basename(respond)
+                        path = os.path.join(basepath, namepath)
+                        print(respond)
+                        try:
+                            data = zf.read(respond)
+                        except IOError as e:
+                            errno, strerror = e.args
+                            print('I/O Error occured! ({0}): {1}'.format(errno, strerror))
+                        else:
+                            with open(path, 'wb') as f:
+                                f.write(zf.read(respond))
