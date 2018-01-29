c192d3bb5d3b830bcf0451c5466527b3d0a09633
padovaSR <34258342+padovaSR@users.noreply.github.com>
2018-01-29 01:43:43 +0100

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+
+def dict_fromFile(text_in):
+    with open(text_in, 'r', encoding='utf-8') as dict_file:
+        new_dict = {}
+        for line in dict_file:
+            x = line.strip().split('=>')
+            if not line:
+                continue
+            key = x[0]
+            value = x[-1]
+            new_dict[key] = value
+            
+    new_dict = dict((k, v) for k, v in new_dict.items() if k)
+    
+    return new_dict
+
 # Dictionary-1
-with open('dictionaries\\Dictionary-1.txt', 'r', encoding='utf-8') as dict_file:
-    dict_1 = {}
-    for line in dict_file:
-        x = line.strip().split('=>')
-        if not line:
-            continue
-        key = x[0]
-        value = x[-1]
-        dict_1[key] = value
-        
-dictionary_1 = dict((k, v) for k, v in dict_1.items() if k)
+dictionary_1 = dict_fromFile(text_in='dictionaries\\Dictionary-1.txt')
+
