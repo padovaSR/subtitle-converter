747cd7ff6d8f54708d203999bf618d93317d1c0e
padovaSR <34258342+padovaSR@users.noreply.github.com>
2018-02-03 04:05:41 +0100

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -9,13 +9,13 @@ def dict_fromFile(text_in, delim):
             x = line.strip().split(delim)
             if not line:
                 continue
-            key = x[0]
-            value = x[-1]
+            if x[0]:
+                key = x[0]
+            if x[-1]:
+                value = x[-1]
             new_dict[key] = value
             
-    new_dict = dict((k, v) for k, v in new_dict.items() if k)
-    
-    return new_dict
+        return new_dict
 
 # Dictionary-1
 dictionary_1 = dict_fromFile(text_in='dictionaries\\Dictionary-1.txt', delim='=>')
