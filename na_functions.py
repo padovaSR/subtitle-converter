d1a6317425903d38ac34e6f6f7e031035379aa46
padovaSR <34258342+padovaSR@users.noreply.github.com>
2018-10-24 02:30:53 +0200

    Update na_functions.py

--- na_functions.py
+++ na_functions.py
@@ -7,6 +7,7 @@ import os
 import pysrt
 import pickle
 import wx
+from textwrap import TextWrapper
 
 from dictionaries import dictionary_0, dictionary_1, dictionary_2, specialReplace
 
@@ -56,7 +57,6 @@ def fixGaps(filein, kode):
 
     return k, pfp
      
-
 class fileOpened:
     def isCompressed(self, infile):
         basepath = os.path.dirname(infile)
@@ -111,4 +111,95 @@ class fileProcessed:
             s1 = re.sub(for_repls, '', s)
             
         self.writeTempStr(intext, s1, kode) 
+        
+    def line_adjust(self, intext):
+        # function for text wrapping and filling
+        def myWrapper(intext):
+            f_rpl = re.compile(r'^((.*?\n.*?){1})\n')
+            n = len(intext) // 2
+            wrapper = TextWrapper(break_long_words=False, break_on_hyphens=False, width=n)
+            te = wrapper.fill(intext)
+            if te.count('\n') >= 2:
+                te = f_rpl.sub(r'\1 ', te)
+            elif te.count('\n') >= 3:
+                te = f_rpl.sub(r'\1 ', te)
+            elif te.count('\n') >= 4:
+                te = f_rpl.sub(r'\n ', te)
+            elif te.count('\n') >= 5:
+                te = f_rpl.sub(r'\1 ', te)
+            elif te.count('\n') == 6:
+                te = f_rpl.sub(r'\1 ', te)
+            return te            
+        # function to move words and characters by its position
+        def movPos(text, n):
+            tagf = re.compile(r'<[^<]*>')
+            # where - pozicija gde se nalazi zeljeni spejs
+            text = text.replace('\n', ' ')
+            where = [m.start() for m in re.finditer(' ', text)][n-1]
+            # before - tekst ispred pozicije
+            before = text[:where]
+            # after - tekst iza pozicije
+            after = text[where:]
+            after = after.replace(' ', '\n', 1)  # zameni prvi spejs
+            newText = before + after
+            return newText
+        
+        n = intext
+        f_rpl = re.compile(r'^((.*?\n.*?){1})\n')
+        s_rpl = re.compile(' +')
+        tag_rpl = re.compile(r'<[^<]*>')
+        new_s = pysrt.SubRipFile()        
+        n = n.replace('\r', '').replace('\n', ' ')
+        n = s_rpl.sub(' ', n)  # vise spejseva u jedan
+        ln = n.replace(',', '').replace('.', '').replace('!', '').replace("'", "")
+        ln = tag_rpl.sub(' ', ln)
+        ln = s_rpl.sub(' ', ln)
+        if len(ln) >= 30:
+            s1 = myWrapper(n)
+            prva = s1.split('\n')[0]
+            druga = s1.split('\n')[-1]
+            druga = druga.replace('i', '').replace('l', '')
+            druga = tag_rpl.sub(' ', druga)
+            druga = s_rpl.sub(' ', druga)                
+            len_prva = len("".join(prva)); len_druga = len("".join(druga))
+            tar1 = len_prva - len_druga
+            tar2 = len_druga - len_prva
+            if tar1 >= 4 or tar2 >= 4:
+                s1 = myWrapper(s1)
+                fls1 = tag_rpl.sub(' ', s1)
+                fls1 = s_rpl.sub(' ', fls1)                        
+                drugaS = len("".join(fls1.split('\n')[-1]))
+                prvaS = len("".join(fls1.split('\n')[0]))
+                if drugaS - prvaS >= 4:
+                    fls1 = tag_rpl.sub(' ', s1)
+                    fls1 = s_rpl.sub(' ', fls1)
+                    lw = fls1.split('\n')[-1].split()
+                    dw = [len(x) for x in lw]           # duzine reci u listi
+                    c1 = s1.split('\n')[0].count(' ') + 1
+                    if len(lw) >= 1:
+                        if (dw[0] + prvaS) <= (drugaS - dw[0]) + 2:
+                            c = c1 + 1
+                            s1 = movPos(s1, c)
+                            c1 = s1.split('\n')[0].count(' ') + 1
+                            fls1 = tag_rpl.sub(' ', s1)
+                            fls1 = s_rpl.sub(' ', fls1)                                 
+                            drugaS = len("".join(s1.split('\n')[-1]))
+                            prvaS = len("".join(s1.split('\n')[0]))
+                            lw = s1.split('\n')[-1].split()
+                            dw = [len(x) for x in lw]                            
+                            if len(lw) >= 2:
+                                if (dw[1] + prvaS) <= (drugaS - dw[1]) + 1:
+                                    c = c1 + 1
+                                    s1 = movPos(s1, c)
+                                    c1 = s1.split('\n')[0].count(' ') + 1
+                                    fls1 = tag_rpl.sub(' ', s1)
+                                    fls1 = s_rpl.sub(' ', fls1)                                         
+                                    drugaS = len("".join(fls1.split('\n')[-1]))
+                                    prvaS = len("".join(fls1.split('\n')[0]))                            
+                sub = s1
+            else:
+                sub = s1
+        else:
+            sub = n
+        return sub
         
