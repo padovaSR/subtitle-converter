d58fa7c35ed182141eb1386ad9c8861222ad1733
padovaSR <g373en@gmail.com>
2019-10-15 21:19:44 +0200

    updated

--- merge.py
+++ merge.py
@@ -18,35 +18,33 @@
 
 import re
 import os
-import sys
+from io import StringIO  
 import pysrt
 from itertools import zip_longest 
 import logging
-from logging.handlers import RotatingFileHandler
+
+from FileProcessing import FileProcessed
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
-handler = RotatingFileHandler(os.path.join('resources', 'var', 'FileProcessing.log'), mode='a', maxBytes=4000, encoding="utf-8")
+handler = logging.FileHandler(filename=os.path.join("resources", "var", "FileProcessing.log"), mode="a", encoding="utf-8")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-def myMerger(file_in, file_out, max_time, max_char, _gap, kode):
-    try:
-        subs = pysrt.open(file_in, encoding=kode)
-    except IOError as e:
-        logger.debug("Merger, I/O error({0}): {1}".format(e.errno, e.strerror))
-    except Exception as e: #handle other exceptions such as attribute errors
-        logger.debug("Merger, unexpected error: {0}".format(sys.exc_info()))    
+def myMerger(subs_in, file_out, max_time, max_char, _gap, kode):
+    
+    subs = subs_in
+    
     if not len(subs) % 2 == 0:
         dsub = pysrt.SubRipItem(subs[-1].index+1, subs[-1].start+6000, subs[-1].end+11000, 'Darkstar test appliance')
         subs.append(dsub)
     else:
         dsub = None
-    
+
     parni = [x for x in subs[1::2]]
     neparni = [x for x in subs[0::2]]
-        
+
     def merge_lines(inPar, inNepar):
         re_pattern = re.compile(r'<[^<]*>')
         new_j = pysrt.SubRipFile()
@@ -70,49 +68,37 @@ def myMerger(file_in, file_out, max_time, max_char, _gap, kode):
                 sub2 = pysrt.SubRipItem(second.index, second.start, second.end, second.text)
                 new_j.append(sub1)
                 new_j.append(sub2)
-                
+
         if dsub in new_j:
             new_j.remove(dsub)
         new_j.clean_indexes()
-        
+
         parni = [x for x in new_j[1::2]]
         neparni = [x for x in new_j[0::2]]        
-        
+
         return new_j, parni, neparni
-    
+
     out_f, par1, nep1 = merge_lines(parni, neparni)
     out_f, par2, nep2 = merge_lines(par1, nep1)
     out_f, par3, nep3 = merge_lines(par2, nep2)
     out_f, par4, nep4 = merge_lines(par3, nep3)
-    
-    out_f.save(file_out, encoding=kode)
-        
 
-def fixIndexes(infile, kode): 
-    subs = pysrt.open(infile, encoding=kode)
+    out_f.save(file_out, encoding=kode)
+
+def fixGaps(path, enc):
     
-    # Fix Index and trim white spaces
-    for i in range(len(subs)):
-        subs[i].index = i + 1
-        
-    subs.save(infile, encoding=kode)
-    
-   
-def fixGaps(filein, kode):
-    
-    subs = pysrt.open(filein, encoding=kode)
-    
+    subs = pysrt.open(path, encoding=enc)
     new_j = pysrt.SubRipFile()
     k = 0
     pfp = 0
     for first, second in zip(subs, subs[1:]):
-        
+
         t1 = first.end.ordinal
         t2 = second.start.ordinal
-        
+
         if t1 > t2:
             pfp += 1
-        
+
         if t1 > t2 or t2 - t1 < 85:
             k += 1
             #first.shift(milliseconds=-25)
@@ -126,13 +112,13 @@ def fixGaps(filein, kode):
     sub = pysrt.SubRipItem(subs[-1].index, subs[-1].start, subs[-1].end, subs[-1].text)
     new_j.append(sub)
     new_j.clean_indexes()
-    
+
     new_f = pysrt.SubRipFile()
     fsub = pysrt.SubRipItem(new_j[0].index, new_j[0].start, new_j[0].end, new_j[0].text)
     new_f.append(fsub)
 
     for first, second in zip(new_j, new_j[1:]):
-        
+
         t1 = first.end.ordinal
         t2 = second.start.ordinal
         if t2 - t1 < 100:
@@ -141,11 +127,11 @@ def fixGaps(filein, kode):
             new_f.append(subf)
         else:
             subf = pysrt.SubRipItem(second.index, second.start, second.end, second.text)
-            new_f.append(subf)        
+            new_f.append(subf)
     
     new_f.clean_indexes()
-    new_f.save(filein, encoding=kode)
-#    new_j.save(filein, encoding=kode)
+    new_f.save(path, encoding=enc)
+    
     return k, pfp
 
 def fixLast(infile, kode):
@@ -154,7 +140,7 @@ def fixLast(infile, kode):
     s2 = (subs[-2].start, subs[-2].end, subs[-2].text)
     s3 = (subs[-3].start, subs[-3].end, subs[-3].text)
     s4 = (subs[-4].start, subs[-4].end, subs[-4].text)
-    
+
     if s4 == s3 and s3 == s2 and s2 == s1:
         subs.remove(subs[-4])
         subs.remove(subs[-3])
