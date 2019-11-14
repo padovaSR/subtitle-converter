bc7822cddb26ac16040c27b37df024bcc9897d5d
padovaSR <g373en@gmail.com>
2019-11-14 21:29:16 +0100

    updated

--- settings.py
+++ settings.py
@@ -5,10 +5,17 @@
 from io import StringIO
 import os
 
+def filePath(*args): return os.path.join(*args)
+
 WORK_TEXT = StringIO()
     
 WORK_SUBS = StringIO()
 
-def filePath(*args): return os.path.join(*args)
+TEXT = r""
+
+ENT_ENCODING = ""
+
+NEW_ENCODING = StringIO()
     
-    
+# shelve : key1=fixer, key2=merger, key3=PATHs, key4=font_data, key5=files_settings
+
