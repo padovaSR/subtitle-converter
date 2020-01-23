5fa1a4d5d7908757e6bd7dfbfd20b8209dde17ff
padovaSR <g373en@gmail.com>
2020-01-23 19:24:02 +0100

    updated

--- showError.py
+++ showError.py
@@ -27,9 +27,14 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
-handler = logging.FileHandler(filename=os.path.join("resources", "var", "File_Errors.log"), mode="a", encoding="utf-8")
+handler = logging.FileHandler(
+    filename=os.path.join("resources", "var", "File_Errors.log"),
+    mode="a",
+    encoding="utf-8",
+)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
 
 def unifiedL(origList, errorList):
     newList = []
@@ -39,29 +44,32 @@ def unifiedL(origList, errorList):
         else:
             newList.append(i)
     return newList
-        
+
+
 def w_position(_pattern, intext):
     intext = intext.replace('\r', '')
-    l1 = []; l2 = []
+    l1 = []
+    l2 = []
     for match in re.finditer(_pattern, intext):
         # group=match.group()
-        begin=match.start()
-        end=match.end()
+        begin = match.start()
+        end = match.end()
         l1.append(begin)
         l2.append(end)
-        
-    f_list = [(x,y) for x, y in zip(l1, l2)]
+
+    f_list = [(x, y) for x, y in zip(l1, l2)]
     return f_list
 
+
 def showMeError(infile, in_text, outfile, kode):
-    
+
     with open(os.path.join('resources', 'var', 'fixer_cb3.data'), 'rb') as f:
         cb3_s = pickle.load(f)
-    
+
     subs = pysrt.from_string(in_text)
-    
+
     if len(subs) > 0:
-        
+
         st = "LINIJE SA GREŠKAMA:\n"
         if kode == 'windows-1251':
             st = "ЛИНИЈЕ СА ГРЕШКАМА:\n"
@@ -82,13 +90,18 @@ def showMeError(infile, in_text, outfile, kode):
             try:
                 sl.save(outfile, encoding=kode)
             except IOError as e:
-                logger.debug("ErrorFile, I/O error({0}): {1}".format(e.errno, e.strerror))
-            except Exception as e: #handle other exceptions such as attribute errors
-                logger.debug("ErrorFile, unexpected error:", sys.exc_info()[0:2])        
-            
-            if os.path.isfile(outfile): logger.debug(f"ErrorFile: {outfile}")
-            if cb3_s == True: webbrowser.open(outfile)
+                logger.debug(
+                    "ErrorFile, I/O error({0}): {1}".format(e.errno, e.strerror)
+                )
+            except Exception as e:  # handle other exceptions such as attribute errors
+                logger.debug("ErrorFile, unexpected error:", sys.exc_info()[0:2])
+
+            if os.path.isfile(outfile):
+                logger.debug(f"ErrorFile: {outfile}")
+            if cb3_s == True:
+                webbrowser.open(outfile)
     else:
         logger.debug(f'showMeError: No subtitles found in {os.path.basename(infile)}')
+
 
 # showMeError(test, outf, kode="utf-8")
