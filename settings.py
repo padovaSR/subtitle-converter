b82d05be280983c3a8e73fe5a50393f3938b547e
padovaSR <g373en@gmail.com>
2020-06-28 22:15:49 +0200

    updated

--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@
 from io import StringIO
 import os
 import re
+import pickle
 
 def filePath(*args): return os.path.join(*args)
 
@@ -24,6 +25,8 @@ WORK_TEXT = StringIO()
     
 WORK_SUBS = StringIO()
 
+with open(os.path.join('resources', 'var', 'obsE.pkl'), 'rb') as f:
+    kodek = pickle.load(f).strip()
 
 
 # shelve : key1=fixer, key2=merger, key3=PATHs, key4=font_data, key5=files_settings
