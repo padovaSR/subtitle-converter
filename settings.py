6d78e9c690ed8c5f0b54f0e0990e0fa073d6f8d0
padovaSR <g373en@gmail.com>
2020-07-01 03:31:27 +0200

    updated

--- settings.py
+++ settings.py
@@ -19,6 +19,10 @@ def printEncoding(entered_enc):
 
 chreg = re.compile("¬")
 
+log_file_history = os.path.join("resources","var","log","file.history.log")
+
+FILE_HISTORY = []
+
 PREVIOUS = []
 
 WORK_TEXT = StringIO()
@@ -28,6 +32,12 @@ WORK_SUBS = StringIO()
 with open(os.path.join('resources', 'var', 'obsE.pkl'), 'rb') as f:
     kodek = pickle.load(f).strip()
 
+with open(log_file_history, encoding="utf-8") as f:
+    for line in f:
+        line = line.strip()
+        if not line:
+            continue
+        FILE_HISTORY.append(line)
 
 # shelve : key1=fixer, key2=merger, key3=PATHs, key4=font_data, key5=files_settings
 
