32673d09660103ff4f6b670ad042fccf0afc1b49
padovaSR <g373en@gmail.com>
2020-07-01 23:13:07 +0200

    updated

--- settings.py
+++ settings.py
@@ -39,19 +39,25 @@ with open(log_file_history, encoding="utf-8") as f:
             continue
         FILE_HISTORY.append(line)
 
-with open(os.path.join("resources","var","presuffix_list.bak"), 'r', encoding='utf-8') as l:
-    added = [line.strip("\n") for line in l if line]
-    
-with open(os.path.join("resources", "var", "file_ext.pkl"), "rb") as f:
-    ex = pickle.load(f)
-    value2_s = ex['cyr_utf8_txt']
-    value5_s = ex['lat_utf8_srt']
+def preSuffix():
+    ''''''
+    with open(
+        os.path.join("resources", "var", "presuffix_list.bak"), 'r', encoding='utf-8'
+    ) as l:
+        added = [line.strip("\n") for line in l if line]
 
-with open(os.path.join("resources", "var", "m_extensions.pkl"), "rb") as f:
-    value_m = pickle.load(f)  #  Merger suffix
+    with open(os.path.join("resources", "var", "file_ext.pkl"), "rb") as f:
+        ex = pickle.load(f)
+        value2_s = ex['cyr_utf8_txt']
+        value5_s = ex['lat_utf8_srt']
 
-with open(os.path.join('resources', 'var', 'tcf.pkl'), 'rb') as tf:
-    oformat = pickle.load(tf)  # TXT suffix
+    with open(os.path.join("resources", "var", "m_extensions.pkl"), "rb") as f:
+        value_m = pickle.load(f)  #  Merger suffix
+
+    with open(os.path.join('resources', 'var', 'tcf.pkl'), 'rb') as tf:
+        oformat = pickle.load(tf)  # TXT suffix
+
+    return added, ex, value2_s, value5_s, value_m, oformat
 
 # shelve : key1=fixer, key2=merger, key3=PATHs, key4=font_data, key5=files_settings
 
