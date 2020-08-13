5961f13a0c6f867fb831f52b7b1845f79aa19135
padovaSR <g373en@gmail.com>
2020-08-13 09:44:59 +0200

    updated

--- merger_settings.py
+++ merger_settings.py
@@ -42,10 +42,7 @@ class Settings(wx.Dialog):
         # self.spin_ctrl_1.SetValue(lineLenght)
 
         self.label_2 = wx.StaticText(
-            self,
-            wx.ID_ANY,
-            "Broj znakova spojenih linija:",
-            style=wx.ALIGN_LEFT,
+            self, wx.ID_ANY, "Broj znakova spojenih linija:", style=wx.ALIGN_LEFT,
         )
         self.spin_ctrl_2 = wx.SpinCtrl(
             self, wx.ID_ANY, style=wx.SP_ARROW_KEYS | wx.TE_PROCESS_ENTER
@@ -54,10 +51,7 @@ class Settings(wx.Dialog):
         # self.spin_ctrl_2.SetValue(maxChar)
 
         self.label_3 = wx.StaticText(
-            self,
-            wx.ID_ANY,
-            "Dozvoljeni gap između linija (ms):",
-            style=wx.ALIGN_LEFT,
+            self, wx.ID_ANY, "Dozvoljeni gap između linija (ms):", style=wx.ALIGN_LEFT,
         )
         self.spin_ctrl_3 = wx.SpinCtrl(
             self, wx.ID_ANY, style=wx.SP_ARROW_KEYS | wx.TE_PROCESS_ENTER
@@ -68,9 +62,7 @@ class Settings(wx.Dialog):
         self.label_4 = wx.StaticText(
             self, wx.ID_ANY, "Sufiks novog fajla:", style=wx.ALIGN_LEFT
         )
-        self.tctrl_1 = wx.TextCtrl(
-            self, wx.ID_ANY, "", style=wx.TE_PROCESS_ENTER
-        )
+        self.tctrl_1 = wx.TextCtrl(self, wx.ID_ANY, "", style=wx.TE_PROCESS_ENTER)
         # self.tctrl_1.SetValue(suffix)
 
         self.static_line_1 = wx.StaticLine(self, wx.ID_ANY)
@@ -78,8 +70,7 @@ class Settings(wx.Dialog):
         self.button_2 = wx.Button(self, wx.ID_SAVE, "")
 
         with shelve.open(
-            os.path.join('resources', 'var', 'dialog_settings.db'),
-            flag='writeback',
+            os.path.join('resources', 'var', 'dialog_settings.db'), flag='writeback',
         ) as sp:
             ex = sp['key2']
             try:
@@ -108,56 +99,42 @@ class Settings(wx.Dialog):
         _icon = wx.NullIcon
         _icon.CopyFromBitmap(
             wx.Bitmap(
-                os.path.join("resources", "icons", "tool.ico"),
-                wx.BITMAP_TYPE_ANY,
+                os.path.join("resources", "icons", "tool.ico"), wx.BITMAP_TYPE_ANY,
             )
         )
         self.SetIcon(_icon)
         self.SetSize((301, 282))
         self.label_1.SetMinSize((123, 23))
-        self.label_1.SetFont(
-            wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.NORMAL, 0, "Segoe UI")
-        )
+        _fonts = wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.NORMAL, 0, "Segoe UI")
+
+        self.label_1.SetFont(_fonts)
 
         self.spin_ctrl_1.SetMinSize((115, 23))
-        self.spin_ctrl_1.SetFont(
-            wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.NORMAL, 0, "Segoe UI")
-        )
+        self.spin_ctrl_1.SetFont(_fonts)
+
         self.spin_ctrl_1.SetToolTip("Maksimalna dužina linije")
 
         self.label_2.SetMinSize((123, 23))
-        self.label_2.SetFont(
-            wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.NORMAL, 0, "Segoe UI")
-        )
+        self.label_2.SetFont(_fonts)
 
         self.spin_ctrl_2.SetMinSize((115, 23))
-        self.spin_ctrl_2.SetFont(
-            wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.NORMAL, 0, "Segoe UI")
-        )
-        self.spin_ctrl_2.SetToolTip(
-            "Maksimalni broj znakova\nspojenih linija"
-        )
+        self.spin_ctrl_2.SetFont(_fonts)
+
+        self.spin_ctrl_2.SetToolTip("Maksimalni broj znakova\nspojenih linija")
         self.label_3.SetMinSize((123, 23))
 
-        self.label_3.SetFont(
-            wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.NORMAL, 0, "Segoe UI")
-        )
+        self.label_3.SetFont(_fonts)
 
         self.spin_ctrl_3.SetMinSize((115, 23))
-        self.spin_ctrl_3.SetFont(
-            wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.NORMAL, 0, "Segoe UI")
-        )
+        self.spin_ctrl_3.SetFont(_fonts)
+
         self.spin_ctrl_3.SetToolTip("Dozvoljeni gap između linija")
 
         self.label_4.SetMinSize((123, 23))
-        self.label_4.SetFont(
-            wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.NORMAL, 0, "Segoe UI")
-        )
+        self.label_4.SetFont(_fonts)
 
         self.tctrl_1.SetMinSize((115, 23))
-        self.tctrl_1.SetFont(
-            wx.Font(9, wx.DEFAULT, wx.NORMAL, wx.NORMAL, 0, "Segoe UI")
-        )
+        self.tctrl_1.SetFont(_fonts)
 
         self.button_1.SetMinSize((86, 24))
         self.button_2.SetMinSize((86, 24))
@@ -166,24 +143,16 @@ class Settings(wx.Dialog):
 
     def __do_layout(self):
         # begin wxGlade: Settings.__do_layout
-        self.sizer_4 = wx.StaticBoxSizer(
-            wx.StaticBox(self, wx.ID_ANY, ""), wx.VERTICAL
-        )
+        self.sizer_4 = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, ""), wx.VERTICAL)
         sizer_5 = wx.BoxSizer(wx.VERTICAL)
         self.sizer_6 = wx.BoxSizer(wx.HORIZONTAL)
         self.grid_sizer_1 = wx.FlexGridSizer(5, 2, 2, 2)
         self.grid_sizer_1.Add(self.label_1, 1, wx.ALL | wx.EXPAND, 3)
-        self.grid_sizer_1.Add(
-            self.spin_ctrl_1, 0, wx.ALIGN_RIGHT | wx.ALL, 3
-        )
+        self.grid_sizer_1.Add(self.spin_ctrl_1, 0, wx.ALIGN_RIGHT | wx.ALL, 3)
         self.grid_sizer_1.Add(self.label_2, 1, wx.ALL | wx.EXPAND, 3)
-        self.grid_sizer_1.Add(
-            self.spin_ctrl_2, 0, wx.ALIGN_RIGHT | wx.ALL, 3
-        )
+        self.grid_sizer_1.Add(self.spin_ctrl_2, 0, wx.ALIGN_RIGHT | wx.ALL, 3)
         self.grid_sizer_1.Add(self.label_3, 1, wx.ALL | wx.EXPAND, 3)
-        self.grid_sizer_1.Add(
-            self.spin_ctrl_3, 0, wx.ALIGN_RIGHT | wx.ALL, 3
-        )
+        self.grid_sizer_1.Add(self.spin_ctrl_3, 0, wx.ALIGN_RIGHT | wx.ALL, 3)
         self.grid_sizer_1.Add(self.label_4, 1, wx.ALL | wx.EXPAND, 3)
         self.grid_sizer_1.Add(self.tctrl_1, 0, wx.ALIGN_RIGHT | wx.ALL, 3)
         self.grid_sizer_1.AddGrowableRow(0)
@@ -194,28 +163,16 @@ class Settings(wx.Dialog):
         self.grid_sizer_1.AddGrowableCol(0)
         self.sizer_4.Add(self.grid_sizer_1, 1, wx.ALL | wx.EXPAND, 3)
         sizer_5.Add(
-            self.static_line_1,
-            0,
-            wx.BOTTOM | wx.EXPAND | wx.LEFT | wx.RIGHT,
-            3,
+            self.static_line_1, 0, wx.BOTTOM | wx.EXPAND | wx.LEFT | wx.RIGHT, 3,
         )
         self.sizer_6.Add(
-            (20, 20),
-            1,
-            wx.ALL | wx.EXPAND,
-            3,
+            (20, 20), 1, wx.ALL | wx.EXPAND, 3,
         )
         self.sizer_6.Add(
-            self.button_1,
-            1,
-            wx.ALL | wx.EXPAND,
-            3,
+            self.button_1, 1, wx.ALL | wx.EXPAND, 3,
         )
         self.sizer_6.Add(
-            self.button_2,
-            1,
-            wx.ALL | wx.EXPAND, 
-            3,
+            self.button_2, 1, wx.ALL | wx.EXPAND, 3,
         )
         sizer_5.Add(self.sizer_6, 1, wx.ALL | wx.EXPAND, 3)
         self.sizer_4.Add(sizer_5, 0, wx.EXPAND, 0)
@@ -235,8 +192,7 @@ class Settings(wx.Dialog):
         c = konf[2]
         d = konf[3]  # ; e = konf[4]; f = konf[5]; g = konf[6]; h = konf[7]
         with shelve.open(
-            os.path.join('resources', 'var', 'dialog_settings.db'),
-            flag='writeback',
+            os.path.join('resources', 'var', 'dialog_settings.db'), flag='writeback',
         ) as s:
             s['key2'] = {
                 'l_lenght': a,
@@ -245,9 +201,7 @@ class Settings(wx.Dialog):
                 'f_suffix': d,
             }
 
-        with open(
-            os.path.join("resources", "var", "m_extensions.pkl"), "wb"
-        ) as f:
+        with open(os.path.join("resources", "var", "m_extensions.pkl"), "wb") as f:
             pickle.dump(file_suffix, f)
 
             self.Destroy()
