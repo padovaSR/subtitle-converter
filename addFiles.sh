b6c7cfeb2904c8eb8ff26fc2fdafe758000aef94
padovaSR <g373en@gmail.com>
2020-08-15 14:46:48 +0200

    new file

---
+++ addFiles.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+default=updated
+if	[	-z "$1"	];	then
+  echo -e "\nUsage $0 file1 file2 ...\n"
+  exit
+else
+  for i in "$@"
+  do
+    target=${i%/}
+    if [[ -f "$target" ]] ; then
+      echo ""
+      echo Add "$i"
+      echo ""
+      git add "$i"
+      echo ""
+      read -p "message: [$default] ": var
+      var=${var:-$default}
+      echo $var
+      git commit -m "$var"
+    fi
+  done
+  echo ""
+  echo Push
+  echo ""
+  git push
+fi
